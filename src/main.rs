29a8d5fece850e1cddc76bb341a1693b0a2ba338
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-12-09 18:24:00 -0800

    Set up Ironworks

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod fairings;
 mod routes;
 mod types;
 
+use std::path::Path;
+use std::sync::Arc;
+
 use crate::db::*;
 use crate::fairings::metrics::*;
 use crate::routes::least_recently_updated::*;
@@ -13,11 +16,26 @@ use crate::routes::metrics::*;
 use crate::routes::most_recently_updated::*;
 use crate::routes::recently_updated::*;
 use crate::routes::tax_rates::*;
+use dotenv_codegen::dotenv;
+use ironworks::ffxiv::Mapper;
+use ironworks::{
+    excel::Excel,
+    ffxiv::{FsResource, Language},
+    sqpack::SqPack,
+    Ironworks,
+};
 use rocket::fairing::AdHoc;
 use rocket_db_pools::Database;
 
 #[launch]
 fn rocket() -> _ {
+    let ffxiv_path = Path::new(&dotenv!("UNIVERSALIS_GAME"));
+    let ironworks = Ironworks::new().with_resource(SqPack::new(FsResource::at(&ffxiv_path)));
+    let mapper = Mapper::new();
+    let excel = Excel::with()
+        .language(Language::English)
+        .build(Arc::new(ironworks), mapper);
+
     rocket::build()
         .attach(Stats::init())
         .attach(TaxRates::init())
@@ -27,6 +45,7 @@ fn rocket() -> _ {
                 println!("Application started");
             })
         }))
+        .manage(excel)
         .mount(
             "/",
             routes![
